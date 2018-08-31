ebc8ef9ff8d00c1b34ffdf9a49890fa41c2d843f
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-08-31 16:07:09 +0200

    function input always between brackets

--- examples/MATLAB/raincloud_plots_tutorial_legacy.m
+++ examples/MATLAB/raincloud_plots_tutorial_legacy.m
@@ -38,7 +38,8 @@ means     = cellfun(@mean, d);
 variances = cellfun(@std, d);
 
 %% Let’s create a quick bar graph of these data. This is the kind of standard visualization you see in many papers, depicting the mean of the data plus standard deviation:
-f1 = figure('Position',fig_position); hold on;
+f1 = figure('Position',fig_position); 
+hold('on');
 h = bar(means, 'FaceColor', 'flat', 'LineWidth',.9);
 
 h(1).CData(1,:) = colours(1,:);
@@ -67,12 +68,12 @@ subplot(2,1,1);
 h1= raincloud_plot('X', d{1}, 'box_on', 1);
 title('Raincloud Plot: Group 1');
 set(gca,'XLim', [0 40]);
-box off;
+box('off');
 subplot(2,1,2)
 h2= raincloud_plot('X', d{2}, 'box_on', 1);
 title('Raincloud Plot: Group 2');
 set(gca,'XLim', [0 40]);
-box off;
+box('off');
 print(f3, [fig_dir, '\3Rain1.png'], '-dpng')
 
 %% This gives us the distribution (probability density plot), summary data (box plot), and raw observations all in one place. Now we’ll walk you through some of the options of the function, which you can use to change various aesthetic properties of the plot. The function only requires a vector of the data you want to plot as the input. Additionally, there are a variety of optional flags you can call to turn the boxplots on and off, to alter ('dodge') the position of the boxes and dots, and to change various aesthetics such as linewidth, colors, and so on. For example, by setting a few different flags we can create more colorful plots:
@@ -81,13 +82,13 @@ subplot(2,1,1);
 h1= raincloud_plot('X', d{1}, 'box_on', 1);
 title('Raincloud Plot: Default Plot');
 set(gca,'XLim', [0 40]);
-box off;
+box('off');
 subplot(2,1,2)
 h2= raincloud_plot('X', d{1}, 'box_on', 1, 'box_dodge', 1, 'box_dodge_amount',...
 0, 'dot_dodge_amount', .3, 'color', cb(1,:), 'cloud_edge_col', cb(1,:));
 title('Raincloud Plot: Some Aesthetic Options');
 set(gca,'XLim', [0 40]);
-box off;
+box('off');
 print(f4, [fig_dir, '\4Rain2.png'], '-dpng');
 
 %% The function returns a cell array for various figure parts, so you can also call the base function and then change things with normal set commands, like so:
@@ -96,14 +97,14 @@ subplot(2,1,1);
 h1= raincloud_plot('X', d{1}, 'box_on', 1);
 title('Raincloud Plot: Default Plot');
 set(gca,'XLim', [0 40]);
-box off;
+box('off');
 subplot(2,1,2)
 h2=  raincloud_plot('X', d{1}, 'box_on', 1);
 title('Raincloud Plot: Some Aesthetic Options');
 set(h2{1},'FaceColor', cb(1,:)) % handles 1-6 are the cloud area, scatterpoints, and boxplot elements respectively
 set(h2{2}, 'MarkerEdgeColor', 'red'); % 
 set(gca,'XLim', [0 40]);
-box off;
+box('off');
 print(f5, [fig_dir, '\5Rain3.png'], '-dpng');
 
 %% You can also control the smoothness of the probability density function by calling the 'bandwidth' parameter. Additionally, if you have Cyril Pernet's robust statistics toolbox on your path, you can call the 'rash' function for an alternative kernel density function:
@@ -112,13 +113,13 @@ subplot(2,1,1);
 h1= raincloud_plot('X', d{1}, 'box_on', 1, 'color', cb(1,:), 'bandwidth', .2, 'density_type', 'ks');
 title('Raincloud Plot: Reduced Smoothing, Kernel Density');
 set(gca,'XLim', [0 40]);
-box off;
+box('off');
 subplot(2,1,2);
 % must have: https://github.com/CPernet/Robust_Statistical_Toolbox
 h1= raincloud_plot('X', d{1}, 'box_on', 1, 'color', cb(2,:), 'bandwidth', 1, 'density_type', 'rash');
 title('Raincloud Plot: Rash Density Estimate');
 set(gca,'XLim', [0 40]);
-box off;
+box('off');
 print(f6, [fig_dir, '\6Rain4.png'], '-dpng');
 
 %% Here, we'll use the dot and box dodge options to create an overlapping set of raincloud plots, useful for group comparison. The function can be called repeatedly (e.g., from within a loop) - each iteration will overlay the previous. Note that here we're using the 'alpha' parameter to make the plot area see-through:
@@ -133,7 +134,7 @@ h2= raincloud_plot('X', d{2}, 'box_on', 1, 'color', cb(4,:), 'alpha', 0.5,...
 legend([h1{1} h2{1}], {'Group 1', 'Group 2'});
 title('A) Dodge Options Example 1');
 set(gca,'XLim', [0 40], 'YLim', [-.075 .15]);
-box off;
+box('off');
 
 % example 2
 subplot(1,2,2);
@@ -146,7 +147,7 @@ h2= raincloud_plot('X', d{2}, 'box_on', 1, 'color', cb(4,:), 'alpha', 0.5,...
 legend([h1{1} h2{1}], {'Group 1', 'Group 2'})
 title('B) Dodge Options Example 2');
 set(gca,'XLim', [0 40]);
-box off;
+box('off');
 print(f7, [fig_dir, '\7Rain5.png'], '-dpng');
 
 %% You can also control the jitter and position of the 'raindrops' in the Y-plane by calling the figure handles:
