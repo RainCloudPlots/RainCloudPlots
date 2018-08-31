b07c70d37c6250edc0a53bc44c06e82178f28b36
Tim van Mourik <TimVanMourik@users.noreply.github.com>
2018-08-31 16:10:23 +0200

    all file names within fullfile

--- examples/MATLAB/raincloud_plots_tutorial_legacy.m
+++ examples/MATLAB/raincloud_plots_tutorial_legacy.m
@@ -49,7 +49,7 @@ e = errorbar(1:2, means, variances, '.k', 'LineWidth',.9);
 set(gca, 'XTick', 1:2);
 title('Bar Plot');
 
-print(f1, [fig_dir, '\1bar.png'], '-dpng');
+print(f1, fullfile(fig_dir, '1bar.png'), '-dpng');
 
 %% As you can see, this tells you something about the data, but a lot of really useful and important information is hidden such as the ‘shape’ or distribution of the data and the raw observations themselves. A histogram nicely shows some of what we’re missing:
 f2 = figure('Position', fig_position);
@@ -60,7 +60,7 @@ title('Histogram');
 subplot(1, 2, 2);
 [n2,x2] = hist(d{2}, 30);
 bar(x2, n2, 'FaceColor', colours(2,:), 'EdgeColor', 'none');
-print(f2, [fig_dir, '\2hist.png'], '-dpng');
+print(f2, fullfile(fig_dir, '2hist.png'), '-dpng');
 
 %% However, now we’ve lost the summary data. The raincloud plot tries to bring these elements together in one intuitive plot. You can use the ‘raincloud_plot.m’ function accompanying this tutorial to produce these plots in Matlab:
 f3 = figure('Position', fig_position);
@@ -74,7 +74,7 @@ h2= raincloud_plot('X', d{2}, 'box_on', 1);
 title('Raincloud Plot: Group 2');
 set(gca,'XLim', [0 40]);
 box('off');
-print(f3, [fig_dir, '\3Rain1.png'], '-dpng')
+print(f3, fullfile(fig_dir, '3Rain1.png'), '-dpng')
 
 %% This gives us the distribution (probability density plot), summary data (box plot), and raw observations all in one place. Now we’ll walk you through some of the options of the function, which you can use to change various aesthetic properties of the plot. The function only requires a vector of the data you want to plot as the input. Additionally, there are a variety of optional flags you can call to turn the boxplots on and off, to alter ('dodge') the position of the boxes and dots, and to change various aesthetics such as linewidth, colors, and so on. For example, by setting a few different flags we can create more colorful plots:
 f4 = figure('Position', fig_position);
@@ -89,7 +89,7 @@ h2= raincloud_plot('X', d{1}, 'box_on', 1, 'box_dodge', 1, 'box_dodge_amount',..
 title('Raincloud Plot: Some Aesthetic Options');
 set(gca,'XLim', [0 40]);
 box('off');
-print(f4, [fig_dir, '\4Rain2.png'], '-dpng');
+print(f4, fullfile(fig_dir, '4Rain2.png'), '-dpng');
 
 %% The function returns a cell array for various figure parts, so you can also call the base function and then change things with normal set commands, like so:
 f5 = figure('Position', fig_position);
@@ -105,7 +105,7 @@ set(h2{1},'FaceColor', cb(1,:)) % handles 1-6 are the cloud area, scatterpoints,
 set(h2{2}, 'MarkerEdgeColor', 'red'); % 
 set(gca,'XLim', [0 40]);
 box('off');
-print(f5, [fig_dir, '\5Rain3.png'], '-dpng');
+print(f5, fullfile(fig_dir, '5Rain3.png'), '-dpng');
 
 %% You can also control the smoothness of the probability density function by calling the 'bandwidth' parameter. Additionally, if you have Cyril Pernet's robust statistics toolbox on your path, you can call the 'rash' function for an alternative kernel density function:
 f6 = figure('Position', fig_position);
@@ -120,7 +120,7 @@ h1= raincloud_plot('X', d{1}, 'box_on', 1, 'color', cb(2,:), 'bandwidth', 1, 'de
 title('Raincloud Plot: Rash Density Estimate');
 set(gca,'XLim', [0 40]);
 box('off');
-print(f6, [fig_dir, '\6Rain4.png'], '-dpng');
+print(f6, fullfile(fig_dir, '6Rain4.png'), '-dpng');
 
 %% Here, we'll use the dot and box dodge options to create an overlapping set of raincloud plots, useful for group comparison. The function can be called repeatedly (e.g., from within a loop) - each iteration will overlay the previous. Note that here we're using the 'alpha' parameter to make the plot area see-through:
 % example 1
@@ -148,7 +148,7 @@ legend([h1{1} h2{1}], {'Group 1', 'Group 2'})
 title('B) Dodge Options Example 2');
 set(gca,'XLim', [0 40]);
 box('off');
-print(f7, [fig_dir, '\7Rain5.png'], '-dpng');
+print(f7, fullfile(fig_dir, '7Rain5.png'), '-dpng');
 
 %% You can also control the jitter and position of the 'raindrops' in the Y-plane by calling the figure handles:
 f8 = figure('Position', fig_position);
@@ -158,7 +158,7 @@ h1{2}.YData = repmat(-0.1,number_of_datapoints,1);
 subplot(2,1,2), h2 = raincloud_plot('X',d{2}, 'color', cb(7,:));
 set(gca,'XLim',[0 40]);
 h2{2}.YData = repmat(-0.05,number_of_datapoints,1); 
-print(f8, [fig_dir, '\8Rain6.png'], '-dpng');
+print(f8, fullfile(fig_dir, '8Rain6.png'), '-dpng');
 
 %% For the final example, we'll consider a more complex factorial situation where we have multiple groups and observations. To illustrate this, we'll use a more complex implementation of rainclouds encoded in the 'raincloud_lineplot_2.m' function. You should consider this function an example of more complex applications, rather than a general purpose tool like the raincloud_plot.m function:
 % grab 'repeated_measures_data.csv';
@@ -178,7 +178,7 @@ clz{2} = repmat(cb(1,:),3,1);
 f9 = figure('Position', fig_position);
 h = raincloud_lineplot_2(data, clz, 1, 1);
 set(gca, 'YLim', [-0.3 1.6]);
-print(f9, [fig_dir, '\9RepRain1.png'], '-dpng')
+print(f9, fullfile(fig_dir, '9RepRain1.png'), '-dpng')
 
 %% That's it! Hopefully this guide has shown you how you can customize your Raincloud plots for a variety of different purposes. This concludes our Matlab tutorial!
 
