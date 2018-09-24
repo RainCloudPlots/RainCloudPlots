5a15e73e48ec8ed9b9c65dad72f207fe696887e2
Paula <orchid00@gmail.com>
2018-09-24 23:40:00 +0200

    Style R_rainclouds.R

--- tutorial_R/R_rainclouds.R
+++ tutorial_R/R_rainclouds.R
@@ -1,21 +1,22 @@
-### This script creates an R function to generate raincloud plots, then simulates 
-### data for plots. If using for your own data, you only need lines 1-80. 
-### It relies largely on code previously written by David Robinson 
-### (https://gist.github.com/dgrtwo/eb7750e74997891d7c20) and ggplot2 by H Wickham
+### This script creates an R function to generate raincloud plots, then simulates
+### data for plots. If using for your own data, you only need lines 1-80.
+### It relies largely on code previously written by David Robinson
+### (https://gist.github.com/dgrtwo/eb7750e74997891d7c20)
+### and the package ggplot2 by Hadley Wickham
 
 
-#check if required packages are installed
-packages <- c("cowplot", "readr", "ggplot2" , "dplyr", "lavaan", "smooth", "Hmisc")
+# Check if required packages are installed ----
+packages <- c("cowplot", "readr", "ggplot2", "dplyr", "lavaan", "smooth", "Hmisc")
 if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
-  install.packages(setdiff(packages, rownames(installed.packages())))  
+  install.packages(setdiff(packages, rownames(installed.packages())))
 }
 
-#Load packages
+# Load packages ----
 library(ggplot2)
 library(dplyr)
 library(lavaan)
 
-# Defining the geom_flat_violin function. Note: the below code modifies the 
+# Defining the geom_flat_violin function. Note: the below code modifies the
 # existing github page by removing a parenthesis in line 50
 
 "%||%" <- function(a, b) {
@@ -47,44 +48,47 @@ geom_flat_violin <- function(mapping = NULL, data = NULL, stat = "ydensity",
 #' @export
 GeomFlatViolin <-
   ggproto("GeomFlatViolin", Geom,
-          setup_data = function(data, params) {
-            data$width <- data$width %||%
-              params$width %||% (resolution(data$x, FALSE) * 0.9)
-            
-            # ymin, ymax, xmin, and xmax define the bounding rectangle for each group
-            data %>%
-              group_by(group) %>%
-              mutate(ymin = min(y),
-                     ymax = max(y),
-                     xmin = x,
-                     xmax = x + width / 2)
-  
-          },
-  
-  draw_group = function(data, panel_scales, coord) {
-    # Find the points for the line to go all the way around
-    data <- transform(data, xminv = x,
-                      xmaxv = x + violinwidth * (xmax - x))
-    
-    # Make sure it's sorted properly to draw the outline
-    newdata <- rbind(plyr::arrange(transform(data, x = xminv), y),
-                     plyr::arrange(transform(data, x = xmaxv), -y))
-    
-    # Close the polygon: set first and last point the same
-    # Needed for coord_polar and such
-    newdata <- rbind(newdata, newdata[1,])
-    
-    ggplot2:::ggname("geom_flat_violin", GeomPolygon$draw_panel(newdata, panel_scales, coord))
-  },
-  
-  draw_key = draw_key_polygon,
-  
-  default_aes = aes(weight = 1, colour = "grey20", fill = "white", size = 0.5,
-                    alpha = NA, linetype = "solid"),
-  
-  required_aes = c("x", "y")
+    setup_data = function(data, params) {
+      data$width <- data$width %||%
+        params$width %||% (resolution(data$x, FALSE) * 0.9)
+
+      # ymin, ymax, xmin, and xmax define the bounding rectangle for each group
+      data %>%
+        group_by(group) %>%
+        mutate(
+          ymin = min(y),
+          ymax = max(y),
+          xmin = x,
+          xmax = x + width / 2
+        )
+    },
+
+    draw_group = function(data, panel_scales, coord) {
+      # Find the points for the line to go all the way around
+      data <- transform(data,
+        xminv = x,
+        xmaxv = x + violinwidth * (xmax - x)
+      )
+
+      # Make sure it's sorted properly to draw the outline
+      newdata <- rbind(
+        plyr::arrange(transform(data, x = xminv), y),
+        plyr::arrange(transform(data, x = xmaxv), -y)
+      )
+
+      # Close the polygon: set first and last point the same
+      # Needed for coord_polar and such
+      newdata <- rbind(newdata, newdata[1, ])
+
+      ggplot2:::ggname("geom_flat_violin", GeomPolygon$draw_panel(newdata, panel_scales, coord))
+    },
+
+    draw_key = draw_key_polygon,
+
+    default_aes = aes(
+      weight = 1, colour = "grey20", fill = "white", size = 0.5,
+      alpha = NA, linetype = "solid"
+    ),
+
+    required_aes = c("x", "y")
   )
-
-
-
-
